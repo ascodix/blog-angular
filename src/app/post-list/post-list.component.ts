c42f42c0bbd07ee4d4ecfbeca91d01ddf865e0cc
ascodix <stephane.albuisson@gmail.com>
2019-03-12 16:01:43 +0100

    2èeme projet

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,20 +1,32 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Post} from '../model/post';
 import {PostService} from '../service/post.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
 
   @Input() posts: Post[] = [];
+  postSubscription: Subscription;
 
   constructor(private postService: PostService) { }
 
   ngOnInit() {
     this. posts = this.postService.posts;
+
+    this.postSubscription = this.postService.postSubject.subscribe(
+      (posts: Post[]) => {
+        this.posts = posts;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.postSubscription.unsubscribe();
   }
 
 }
